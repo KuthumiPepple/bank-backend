9bba48b19483277c02408ad139ae782b47e9a6d4
Kuthumi Pepple <kuthumipepple@gmail.com>
2024-05-29 16:54:42 +0100

    handle request to list accounts

--- api/account.go
+++ api/account.go
@@ -15,19 +15,19 @@ type createAccountRequest struct {
 
 func (server *Server) createAccount(c *gin.Context) {
 	var req createAccountRequest
-	if err := c.ShouldBindJSON(&req); err != nil{
+	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner:    req.Owner,
 		Currency: req.Currency,
-		Balance: 0,
+		Balance:  0,
 	}
 
 	account, err := server.store.CreateAccount(c, arg)
-	if err != nil{
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
@@ -35,20 +35,20 @@ func (server *Server) createAccount(c *gin.Context) {
 	c.JSON(http.StatusCreated, account)
 }
 
-type getAccountRequest struct{
+type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
-func (server *Server) getAccount(c *gin.Context){
+func (server *Server) getAccount(c *gin.Context) {
 	var req getAccountRequest
-	if err := c.ShouldBindUri(&req); err != nil{
+	if err := c.ShouldBindUri(&req); err != nil {
 		c.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	account, err := server.store.GetAccount(c, req.ID)
 	if err != nil {
-		if err == sql.ErrNoRows{
+		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
@@ -58,4 +58,30 @@ func (server *Server) getAccount(c *gin.Context){
 	}
 
 	c.JSON(http.StatusOK, account)
+}
+
+type listAccountsRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) ListAccounts(c *gin.Context) {
+	var req listAccountsRequest
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(c, arg)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, accounts)
 }
--- api/server.go
+++ api/server.go
@@ -16,6 +16,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.ListAccounts)
 
 	server.router = router
 	return server
