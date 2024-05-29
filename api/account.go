2c8b343a7428c5868ebc26eeb5ea2de8553e4e95
Kuthumi Pepple <kuthumipepple@gmail.com>
2024-05-29 16:10:59 +0100

    handle request to get an account by id

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,7 +10,7 @@ import (
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR GBP"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR GBP NGN"`
 }
 
 func (server *Server) createAccount(c *gin.Context) {
@@ -32,4 +33,29 @@ func (server *Server) createAccount(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, account)
+}
+
+type getAccountRequest struct{
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(c *gin.Context){
+	var req getAccountRequest
+	if err := c.ShouldBindUri(&req); err != nil{
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(c, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows{
+			c.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, account)
 }
