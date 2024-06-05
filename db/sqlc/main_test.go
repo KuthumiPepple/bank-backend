c5fd7c92f0ad44c9d1628283f6f4a4163e1d124d
Kuthumi Pepple <kuthumipepple@gmail.com>
2024-06-05 15:44:31 +0100

    load config with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/kuthumipepple/bank-backend/util"
 	_ "github.com/lib/pq"
 )
 
@@ -13,8 +14,11 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open("postgres", "postgresql://root:secret@localhost:5432/bank?sslmode=disable")
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
